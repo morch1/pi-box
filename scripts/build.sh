26a576555f82187d78b9c9372d3d7a5e8408e588
morch1 <70644384+morch1@users.noreply.github.com>
2026-08-09 20:25:05 +0200

    build script changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,9 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
+IMAGE_NAME=${IMAGE_NAME:-"morchv/pi-box"}
+IMAGE_VERSION=${IMAGE_VERSION:-$(jq -r '.version' "$ROOT_DIR/devcontainers/pi-box/devcontainer-template.json")}
+
 REGISTRY_URL=${REGISTRY_URL:-"gitea.home.morch.al"}
 ALPINE_VERSION=${ALPINE_VERSION:-"3.24"}
 NODE_VERSION=${NODE_VERSION:-"26.5.0"}
@@ -27,24 +30,22 @@ if [ "$REBUILD" = true ]; then
   DOCKER_BUILD_ARGS+=(--no-cache)
 fi
 
-VERSION=$(jq -r '.version' "$ROOT_DIR/devcontainers/pi-box/devcontainer-template.json")
-
-echo "Building release version $VERSION"
-docker build "${DOCKER_BUILD_ARGS[@]}" -t morchv/pi-box:"$VERSION" \
+echo "Building release version $IMAGE_VERSION"
+docker build "${DOCKER_BUILD_ARGS[@]}" -t "$IMAGE_NAME:$IMAGE_VERSION" \
   --build-arg ALPINE_VERSION="$ALPINE_VERSION" \
   --build-arg NODE_VERSION="$NODE_VERSION" \
   --build-arg PYTHON_VERSION="$PYTHON_VERSION" \
   --build-arg PI_VERSION="$PI_VERSION" \
-  "$ROOT_DIR/pi-box"
+  "$ROOT_DIR/image"
 
-docker tag morchv/pi-box:"$VERSION" morchv/pi-box:latest
-docker tag morchv/pi-box:"$VERSION" "$REGISTRY_URL/morchv/pi-box:$VERSION"
-docker tag morchv/pi-box:"$VERSION" "$REGISTRY_URL/morchv/pi-box:latest"
+docker tag "$IMAGE_NAME:$IMAGE_VERSION" "$IMAGE_NAME:latest"
+docker tag "$IMAGE_NAME:$IMAGE_VERSION" "$REGISTRY_URL/$IMAGE_NAME:$IMAGE_VERSION"
+docker tag "$IMAGE_NAME:$IMAGE_VERSION" "$REGISTRY_URL/$IMAGE_NAME:latest"
 
 if [ "$RELEASE" = true ]; then
   echo "Pushing images to registry..."
-  docker push "$REGISTRY_URL/morchv/pi-box:$VERSION"
-  docker push "$REGISTRY_URL/morchv/pi-box:latest"
+  docker push "$REGISTRY_URL/$IMAGE_NAME:$IMAGE_VERSION"
+  docker push "$REGISTRY_URL/$IMAGE_NAME:latest"
 
   echo "Publishing devcontainer templates to registry..."
   devcontainer templates publish -r "$REGISTRY_URL" -n morchv/devcontainers devcontainers
