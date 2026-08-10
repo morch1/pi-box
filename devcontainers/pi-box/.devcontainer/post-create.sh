3c5df3c880b04d5667fea1d05b3970fb5cb85d32
morch1 <70644384+morch1@users.noreply.github.com>
2026-08-10 14:44:44 +0200

    auto install dev requirements

--- devcontainers/pi-box/.devcontainer/post-create.sh
+++ devcontainers/pi-box/.devcontainer/post-create.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Running post-create.sh script..."
-
 if [ -f requirements.txt ]; then 
-    echo "Installing Python dependencies from requirements.txt..."
+    echo "Installing production requirements.txt..."
     python -m pip install -r requirements.txt
+fi
+
+if [ -f requirements.dev.txt ]; then 
+    echo "Installing development requirements.txt..."
+    python -m pip install -r requirements.dev.txt
 fi
 
 if [ -f .env.example ] && [ ! -f .env ]; then
